127350f6f0fff340694f2ea281d4f318760646d9
Larisa Shigareva <larisa.shigareva@all.me>
2020-06-07 14:30:24 +0300

    Show current branch for better git navigation

--- bash-cfg.sh
+++ bash-cfg.sh
@@ -3,6 +3,15 @@ alias cde='cd ${HOME}/Desktop'
 alias cdc='cd ${HOME}/box-cfg'
 alias g='git'
 alias ll='gls -l -h -A -F --color --group-directories-first $@'
-export PS1="\\W \\$ "
 export PYTHONDONTWRITEBYTECODE=1
 export PYTHONSTARTUP=~/box-cfg/pythonstartup.py
+
+set_bash_prompt() {
+    branch_name=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    if test $? = "0"; then
+        PS1="\\W (\[\033[0;33m\]$branch_name\[\033[0m\]) \\$ "
+    else
+        PS1="\\W \\$ "
+    fi
+}
+PROMPT_COMMAND=set_bash_prompt
